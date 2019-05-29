0d2c1af4a8372fb19066df88ab61d62752d4f105
jelufe <jeanlucasjlfs@gmail.com>
2019-05-29 14:05:53 -0300

    Adicionado chalk e yargs

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Todo-List");
+const yargs = require('yargs');
+const chalk = require('chalk');
+
+yargs.command({
+    command: 'add',
+    describe: 'Adiciona uma nova Tarefa',
+    handler: function () {
+        console.log(chalk.green('Adicionando a tarefa'));
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove uma Tarefa',
+    handler: function () {
+        console.log(chalk.red('Removendo a tarefa'));
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Lista as Tarefas',
+    handler: function () {
+        console.log(chalk.yellow('Listando as tarefas'));
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Mostra uma Tarefa',
+    handler: function () {
+        console.log(chalk.blue('Mostrando a tarefa'));
+    }
+});
+
+console.log(yargs.argv);
